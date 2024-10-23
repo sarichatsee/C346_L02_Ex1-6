343910d0636c1885d56d4f94654b08e59321e927
fann <22034013@myrp.edu.sg>
2024-10-23 14:17:49 +0800

    HELPP

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome6";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Movie component definition
+const Movie = ({ title, year, poster }) => {
+    return (
+        <View>
+            <Image source={poster} />
+            <Text>
+                {title} {year}
+            </Text>
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// AllMovies component definition
+const AllMovies = () => {
+    return (
+        <View>
+            <Movie
+                title="Doctor Sleep"
+                year="2019"
+                poster={require('./assets/doctor-sleep.jpg')} // Path to the image
+            />
+            <Text>
+                <Icon name="person-rifle" size={20} color="red" />
+            </Text>
+            <Movie
+                title="Midway"
+                year="2020"
+                poster={require('./assets/midway.jpg')} // Path to the image
+            />
+        </View>
+    );
+};
+
+// Main App component
+const App = () => {
+    return (
+        <ScrollView>
+            <AllMovies />
+            <StatusBar style="auto" />
+        </ScrollView>
+    );
+};
+
+export default App;
